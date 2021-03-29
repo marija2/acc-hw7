17c7a4ac8193ae035c60570444d6dea38f9deed9
Marija Trifkovic <mt3786@nyu.edu>
2021-03-29 10:32:12 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+// backend code, server code
+console.log ( "in server.js" );
+
+// sockets
+var express = require ( 'express' );
+
+// using a constructor to create an express app
+var app = express();
+
+// create our server
+var server = app.listen ( 3000 );
+
+// use files in public folder
+app.use ( express.static ( 'public' ) );
+
+var socket = require ( 'socket.io' );
+
+// a variable that keeps track of inputs and outputs
+var io = socket ( server );
+
+io.sockets.on ( 'connection', newConnection );
+
+function newConnection ( socket ) {
+    console.log ( 'new conn ' + socket.id );
+
+    socket.on ( 'mouse', mouseMsg );
+    socket.on ( 'emoji', emojiMsg);
+
+    function mouseMsg ( data ) {
+        socket.broadcast.emit ( 'mouse', data );
+    }
+
+    function emojiMsg ( data ) {
+        socket.broadcast.emit ('emoji', data );
+    }
+
+}
+
+
